f350506886a864cd1e11656465ebc515e95e2d7c
annguyen2803 <nguyenanht2003@gmail.com>
2024-04-27 20:24:48 +0700

    first commit

---
+++ assets/php/orders.php
@@ -0,0 +1,93 @@
+<div class="details">
+                <div class="recentOrders">
+                    <div class="cardHeader">
+                        <h2>Recent Orders</h2>
+                        <a href="#" class="btn">View All</a>
+                    </div>
+                    <table>
+                        <thead>
+                            <td>Name</td>
+                            <td>Price</td>
+                            <td>Payment</td>
+                            <td>Status</td>
+                        </thead>
+                    
+                    <tbody>
+                        <tr>
+                            <td>Star Refrigertor</td>
+                            <td>$1200</td>
+                            <td>Paid</td>
+                            <td><span class="status delivered">Delivered</span></td>
+                        </tr>
+                        <tr>
+                            <td>Star Refrigertor</td>
+                            <td>$1200</td>
+                            <td>Due</td>
+                            <td><span class="status pending">Pending</span></td>
+                        </tr>
+                        <tr>
+                            <td>Dell Laptop</td>
+                            <td>$1200</td>
+                            <td>Paid</td>
+                            <td><span class="status return">Return</span></td>
+                        </tr>
+                        <tr>
+                            <td>Apple Watch</td>
+                            <td>$1200</td>
+                            <td>Paid</td>
+                            <td><span class="status inProgress">In Progress</span></td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+
+                <!-- ============== New Customers =============== -->
+                <div class="recentCustomers">
+                    <div class="cardHeader">
+                        <h2>Recent Customers</h2>
+                    </div>
+
+                    <table>
+                        <tr>
+                            <td style="width: 60px;">
+                                <div class="imgBx">
+                                    <img src="assets/images/" alt="">
+                                </div>
+                            </td>
+                            <td>
+                                <h4>David <br><span>Italy</span></h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 60px;">
+                                <div class="imgBx">
+                                    <img src="assets/images/" alt="">
+                                </div>
+                            </td>
+                            <td>
+                                <h4>David <br><span>Italy</span></h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 60px;">
+                                <div class="imgBx">
+                                    <img src="assets/images/" alt="">
+                                </div>
+                            </td>
+                            <td>
+                                <h4>David <br><span>Italy</span></h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 60px;">
+                                <div class="imgBx">
+                                    <img src="assets/images/" alt="">
+                                </div>
+                            </td>
+                            <td>
+                                <h4>David <br><span>Italy</span></h4>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
